4a25c93f6a04821fc32960c63272b00ab170fba4
StarHubX <project.starhubx@gmail.com>
2024-09-30 16:26:53 +0700

    Create Version_Checker.lua

---
+++ Roblox_Scripts/Version_Checker.lua
@@ -0,0 +1,7 @@
+getgenv().StarHubX = {
+	["Game Lists"] = {
+		[16146832113] = "https://raw.githubusercontent.com/KenzCoder47/Main/script/Roblox_Scripts/Anime_Vanguards/Version.lua",
+		[16277809958] = "https://raw.githubusercontent.com/KenzCoder47/Main/script/Roblox_Scripts/Anime_Vanguards/Version.lua",
+		[18219125606] = "https://raw.githubusercontent.com/KenzCoder47/Main/script/Roblox_Scripts/Anime_Vanguards/Version.lua"
+	}
+}
