ce0ef81b1c2ae1a5d1de212d45984b88f6d3fb83
StarHubX <project.starhubx@gmail.com>
2024-10-13 03:00:31 +0700

    Create Code.lua

---
+++ Roblox_Scripts/King_Legacy/Code.lua
@@ -0,0 +1,12 @@
+local Codes = {
+    "[🟡] FREESTATSRESET",
+    "[🟡] DOUGHAWAKENING",
+    "[🟡] PLAYKINGLEGACYFOR10GEMS",
+    "[🟡] OWNERMETEOR",
+    "[🟡] DinoxLive",
+    "[🟡] Peodiz10k",
+    "[🟡] 2MFAV",
+    "[🟡] Peodiz",
+    "[🟡] WELCOMETOKINGLEGACY"
+}
+return Codes
