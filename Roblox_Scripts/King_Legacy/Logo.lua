f751a30443f0103f9859e1551a153a0a910ef435
StarHubX <project.starhubx@gmail.com>
2024-10-13 02:55:25 +0700

    Create Logo.lua

---
+++ Roblox_Scripts/King_Legacy/Logo.lua
@@ -0,0 +1,16 @@
+local StarHubX = {
+    Icon_Data = {
+        Information    = "rbxassetid://10723415903",
+        Main           = "rbxassetid://10723407389",
+        Premium        = "rbxassetid://10709818626",
+        Farm_Settings  = "rbxassetid://10734950020",
+        Fruit          = "rbxassetid://10709790875",
+        Sea            = "rbxassetid://10734941354",
+        Raid_Boss      = "rbxassetid://10734975692",
+        Shop           = "rbxassetid://10734952479",
+        Teleport       = "rbxassetid://10734886004",
+        Server         = "rbxassetid://10734949856"
+    }
+}
+
+return StarHubX.Icon_Data
