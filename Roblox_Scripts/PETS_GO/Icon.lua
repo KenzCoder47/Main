8bb77de6153275d40925754956f99fc16a09e094
StarHubX <project.starhubx@gmail.com>
2024-10-14 01:57:31 +0700

    Update Icon.lua

--- Roblox_Scripts/PETS_GO/Icon.lua
+++ Roblox_Scripts/PETS_GO/Icon.lua
@@ -1,13 +1,13 @@
 local StarHubX = {
     Icon_Data = {
         Script_Information    = "rbxassetid://86836716655174",
-        Main           = "rbxassetid://118377222156791",
-        Premium        = "rbxassetid://79703287917131",
-        Potion         = "rbxassetid://95484850196554",
-        Fruit          = "rbxassetid://100645334218933",
-        Mail_Box       = "rbxassetid://104317014217430",
-        Webhook        = "rbxassetid://83898715400476",
-        Settings       = "rbxassetid://119234689936339"
+        Main                  = "rbxassetid://118377222156791",
+        Premium               = "rbxassetid://79703287917131",
+        Potion                = "rbxassetid://95484850196554",
+        Fruit                 = "rbxassetid://100645334218933",
+        Mail_Box              = "rbxassetid://104317014217430",
+        Webhook               = "rbxassetid://83898715400476",
+        Settings              = "rbxassetid://119234689936339"
     }
 }
 
